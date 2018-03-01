28e261b9e9ef320d7aab1d786c09af84c4be4118
liuchibing <liuchibing@outlook.com>
2018-03-01 21:27:25 +0800

    adjust layout of EnterRoom

--- src/EnterRoom.jsx
+++ src/EnterRoom.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 
 import 'antd/dist/antd.css'
-import { Layout, Row, Col } from 'antd'
+import { Layout, Row, Col, Input, Button } from 'antd'
 
 class EnterRoom extends Component {
   render () {
@@ -10,7 +10,10 @@ class EnterRoom extends Component {
         <Layout.Content>
           <Row type='flex' justify='center'><Col><h1 style={{ marginTop: '1rem' }}>Tic-Tac-FIR</h1></Col></Row>
           <Row type='flex' justify='center'><Col>
-            <p>Hello</p>
+            <Input size='large' placeholder='要加入的游戏ID' />
+          </Col></Row>
+          <Row type='flex' justify='center'><Col>
+            <Button type='primary' size='large' shape='circle' icon='arrow-right' style={{ marginTop: '0.7rem' }} />
           </Col></Row>
         </Layout.Content>
       </Layout>
