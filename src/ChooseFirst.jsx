0518685d9ecf237314d0897f6328464c11e8910a
liuchibing <liuchibing@outlook.com>
2018-03-02 22:35:51 +0800

    change ChooseFirst page

--- src/ChooseFirst.jsx
+++ src/ChooseFirst.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 
 import 'antd/dist/antd.css'
-import { Layout, Row, Col, Button, Slider } from 'antd'
+import { Layout, Row, Col, Button, Slider, Divider } from 'antd'
 
 class ChooseFirst extends Component {
   render () {
@@ -10,15 +10,21 @@ class ChooseFirst extends Component {
         <Layout.Content>
           <Row type='flex' justify='center'><Col><h1 style={{ marginTop: '1rem' }}>Tic-Tac-FIR</h1></Col></Row>
           {!this.props.isHost
-            ? <Row type='flex' justify='center'><Col style={{ width: '80%' }}>
-              <Slider min={5} max={19} defaultValue={15} marks={{
-                5: '5',
-                10: '10',
-                15: '15',
-                17: '17',
-                19: '19'
-              }} />
-            </Col></Row>
+            ? <div>
+              <Row type='flex' justify='center'><Col>
+                <p style={{ textAlign: 'center' }}>选择棋盘大小：</p>
+              </Col></Row>
+              <Row type='flex' justify='center'><Col style={{ width: '80%' }}>
+                <Slider min={5} max={19} defaultValue={15} marks={{
+                  5: '5',
+                  10: '10',
+                  15: '15',
+                  17: '17',
+                  19: '19'
+                }} />
+              </Col></Row>
+              <Divider />
+            </div>
             : null}
           <Row type='flex' justify='center'>
             <Col>
