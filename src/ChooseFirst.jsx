a8e8af6809fc6bf8af62426f83ecd5a7f2e2fd80
liuchibing <liuchibing@outlook.com>
2018-03-02 19:10:37 +0800

    change choosefirst page

--- src/ChooseFirst.jsx
+++ src/ChooseFirst.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 
 import 'antd/dist/antd.css'
-import { Layout, Row, Col, Button } from 'antd'
+import { Layout, Row, Col, Button, Slider } from 'antd'
 
 class ChooseFirst extends Component {
   render () {
@@ -9,6 +9,17 @@ class ChooseFirst extends Component {
       <Layout>
         <Layout.Content>
           <Row type='flex' justify='center'><Col><h1 style={{ marginTop: '1rem' }}>Tic-Tac-FIR</h1></Col></Row>
+          {!this.props.isHost
+            ? <Row type='flex' justify='center'><Col style={{ width: '80%' }}>
+              <Slider min={5} max={19} defaultValue={15} marks={{
+                5: '5',
+                10: '10',
+                15: '15',
+                17: '17',
+                19: '19'
+              }} />
+            </Col></Row>
+            : null}
           <Row type='flex' justify='center'>
             <Col>
               <div style={{ textAlign: 'center', fontSize: '3rem' }}>{this.props.myRandom || 5}</div>
@@ -16,7 +27,7 @@ class ChooseFirst extends Component {
             </Col>
             <Col>
               <div style={{ textAlign: 'center', fontSize: '3rem' }}>{this.props.peerRandom || 8}</div>
-              <Button size='large' type='primary' disabled style={{ marginRight: '1rem' }}>生成随机数</Button>
+              <Button size='large' type='primary' loading disabled style={{ marginRight: '1rem' }}>生成随机数</Button>
             </Col>
           </Row>
         </Layout.Content>
